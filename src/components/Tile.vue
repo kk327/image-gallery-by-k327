1a75a87f8969085107f1da57af46bde2fc9c7264
k327 <96351133+kk327@users.noreply.github.com>
2024-09-23 20:13:17 +0200

    Add files via upload

---
+++ src/components/Tile.vue
@@ -0,0 +1,62 @@
+<script setup>
+    const props = defineProps([
+        "data"
+    ])
+</script>
+
+<template>
+    <article class="tileMain">
+        <div class="background" :style="{ backgroundImage: 'url(' + data.url + ')' }"></div>
+        <section class="mainTileContent">
+            <img :style="{ content: 'url(' + data.url + ')' }">
+            <p>{{ data.title }}</p>
+        </section>
+    </article>
+</template>
+
+<style scoped>
+    img {
+        height: 250px;
+        width: 250px;
+        object-fit: cover;
+        border-radius: 20px;
+        max-width: 90vw;
+        background-color: rgb(0, 0, 0, 0.3);
+    }
+
+    .tileMain {
+        width: 250px;
+        height: 325px;
+        color: white;
+        max-width: 90vw;
+    }
+
+    .background {
+        filter: blur(100px);
+        position: absolute;
+        width: 275px;
+        height: 325px;
+        z-index: -1;
+        background-size: cover;
+    }
+
+    p {
+        font-weight: 600;
+        height: 60px;
+        position: relative;
+        bottom: 15px;
+        text-align: center;
+        overflow-y: auto;
+        overflow-x: hidden;
+        text-wrap: wrap;
+    }
+
+    .mainTileContent {
+        transition-duration: 200ms;
+    }
+
+    .mainTileContent:hover {
+        scale: 90%;
+        cursor: pointer;
+    }
+</style>
